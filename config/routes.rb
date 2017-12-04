bb82a9abb69892cc3f1a6e628a75dd9f1fff6da1
Aristides Perez <aris.a.perez@gmail.com>
2017-12-04 07:24:02 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   scope :module => 'api' do
     scope :module => 'v1' do
       mount_devise_token_auth_for 'User', :at => 'auth'
-      resources :games, :only => [:index]
+      resources :games, :except => %i[new edit destroy]
       resources :sheets, :except => %i[new edit] do
         resources :boxes, :only => [:update]
       end
