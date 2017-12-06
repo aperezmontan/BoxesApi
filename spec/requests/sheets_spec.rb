a29955cc9089e049b74efe82f64b9b094b90a960
Aristides Perez <aris.a.perez@gmail.com>
2017-12-06 00:27:43 -0500

    Refactor specs

--- spec/requests/sheets_spec.rb
+++ spec/requests/sheets_spec.rb
@@ -3,11 +3,20 @@
 require 'rails_helper'
 
 describe 'Sheets', :type => :request do
+  include_context 'shared auth'
+
   let!(:sheet) { create(:sheet) }
   let!(:sheets) { [sheet] }
 
+  subject do
+    request
+    response
+  end
+
+  let(:headers) { auth_headers }
+  let(:body) { JSON.parse(subject.body) }
+
   describe 'post /sheets' do
-    let(:body) { JSON.parse(response.body) }
     let(:sheet_params) do
       {
         :name => 'new_new_sheet',
@@ -18,22 +27,21 @@ describe 'Sheets', :type => :request do
       }
     end
 
-    subject { post '/sheets', :params => { :sheet => sheet_params } }
-
     context 'with valid params' do
+      let(:request) { post '/sheets', :params => { :sheet => sheet_params }, :headers => headers }
+
+      it_behaves_like 'authorized user endpoint'
+
       it 'creates the sheet' do
         expect { subject }.to change { ::Sheet.count }.by(1)
       end
 
       it 'responds 201 status' do
-        subject
-        expect(response.status).to eq(201)
+        expect(subject.status).to eq(201)
       end
 
       it 'responds with sheet' do
-        subject
-
-        expect(response.content_type).to eq('application/json')
+        expect(subject.content_type).to eq('application/json')
         expect(body['sheet']['home_team']).to eq('home')
         expect(body['sheet']['away_team']).to eq('away')
       end
@@ -41,18 +49,18 @@ describe 'Sheets', :type => :request do
 
     context 'with bad params' do
       context 'without unpermitted params' do
-        it 'ignores unpermitted params' do
-          post '/sheets', :params => { :sheet => sheet_params.merge!(:foo => 'bar') }
+        let(:request) { post '/sheets', :params => { :sheet => sheet_params.merge!(:foo => 'bar') }, :headers => headers }
 
-          expect(response.status).to eq(201)
+        it 'ignores unpermitted params' do
+          expect(subject.status).to eq(201)
         end
       end
 
       context 'with incomplete params' do
-        it 'responds with an error' do
-          post '/sheets', :params => { :sheet => { :home_team => 'home', :user_id => User.first.id } }
+        let(:request) { post '/sheets', :params => { :sheet => { :home_team => 'home', :user_id => User.first.id } }, :headers => headers }
 
-          expect(response.status).to eq(422)
+        it 'responds with an error' do
+          expect(subject.status).to eq(422)
         end
       end
 
@@ -60,19 +68,19 @@ describe 'Sheets', :type => :request do
 
       # TODO: test when Devise is setup
       xcontext 'without a user' do
-        xit 'responds with an error' do
-          post '/sheets', :params => { :sheet => bad_sheet_params.merge!(:game_id => Game.first.id) }
+        let(:request) { post '/sheets', :params => { :sheet => bad_sheet_params.merge!(:game_id => Game.first.id) }, :headers => headers }
 
-          expect(response.status).to eq(422)
+        xit 'responds with an error' do
+          expect(subject.status).to eq(422)
           expect(body).to eq('user' => ['must exist'])
         end
       end
 
       context 'without a game' do
-        it 'responds with an error' do
-          post '/sheets', :params => { :sheet => bad_sheet_params.merge!(:user_id => User.first.id) }
+        let(:request) { post '/sheets', :params => { :sheet => bad_sheet_params.merge!(:user_id => User.first.id) }, :headers => headers }
 
-          expect(response.status).to eq(422)
+        it 'responds with an error' do
+          expect(subject.status).to eq(422)
           expect(body).to eq('game' => ['must exist'])
         end
       end
@@ -81,31 +89,31 @@ describe 'Sheets', :type => :request do
 
   describe 'delete /sheets/:id' do
     context 'with valid sheet' do
-      subject { delete "/sheets/#{sheet.id}" }
+      let(:request) { delete "/sheets/#{sheet.id}", :headers => headers }
+
+      it_behaves_like 'authorized user endpoint'
 
       it 'destroys the sheet' do
         expect { subject }.to change { ::Sheet.count }.by(-1)
       end
 
       it 'responds 204 status' do
-        subject
-        expect(response.status).to eq(204)
+        expect(subject.status).to eq(204)
       end
     end
 
     context 'with invalid sheet' do
-      subject { delete '/sheets/foo' }
+      let(:request) { delete '/sheets/foo', :headers => headers }
 
       it 'responds with an error' do
-        subject
-        expect(response.status).to eq(422)
-        expect(response.content_type).to eq('application/json')
+        expect(subject.status).to eq(422)
+        expect(subject.content_type).to eq('application/json')
       end
     end
   end
 
   describe '/sheets' do
-    before { get '/sheets' }
+    let(:request) { get '/sheets' }
 
     let(:sheets_response) do
       sheets.map do |sheet|
@@ -121,16 +129,16 @@ describe 'Sheets', :type => :request do
     let(:expected_response) { { 'sheets' => sheets_response } }
 
     it 'responds 200 status' do
-      expect(response.status).to eq(200)
+      expect(subject.status).to eq(200)
     end
 
     it 'responds with sheets' do
-      expect(JSON.parse(response.body)).to eq(expected_response)
+      expect(body).to eq(expected_response)
     end
   end
 
   describe 'get /sheets/:id' do
-    before { get "/sheets/#{sheet.id}" }
+    let(:request) { get "/sheets/#{sheet.id}" }
 
     let(:sheets_response) do
       {
@@ -144,11 +152,11 @@ describe 'Sheets', :type => :request do
     let(:expected_response) { { 'sheet' => sheets_response } }
 
     it 'responds 200 status' do
-      expect(response.status).to eq(200)
+      expect(subject.status).to eq(200)
     end
 
     it 'responds with sheet' do
-      expect(JSON.parse(response.body)).to eq(expected_response)
+      expect(body).to eq(expected_response)
     end
   end
 
@@ -170,7 +178,9 @@ describe 'Sheets', :type => :request do
     let(:expected_response) { { 'sheet' => sheets_response } }
 
     context 'with valid params' do
-      subject { put "/sheets/#{sheet.id}", :params => { :sheet => { :home_team => 'NY', :away_team => 'NJ' } } }
+      let(:request) { put "/sheets/#{sheet.id}", :params => { :sheet => { :home_team => 'NY', :away_team => 'NJ' } }, :headers => headers  }
+
+      it_behaves_like 'authorized user endpoint'
 
       it 'updates the sheet' do
         expect(sheet.home_team).to eq('NY')
@@ -182,16 +192,16 @@ describe 'Sheets', :type => :request do
       end
 
       it 'responds with sheet' do
-        expect(JSON.parse(response.body)).to eq(expected_response)
+        expect(body).to eq(expected_response)
       end
     end
 
     context 'with invalid params' do
-      subject { put "/sheets/#{sheet.id}", :params => { :sheet => { :home_team => nil } } }
+      let(:request) { put "/sheets/#{sheet.id}", :params => { :sheet => { :home_team => nil } }, :headers => headers }
 
       it 'responds with an error' do
         expect(response.status).to eq(422)
-        expect(JSON.parse(response.body)).to eq('home_team' => ["can't be blank"])
+        expect(body).to eq('home_team' => ["can't be blank"])
       end
     end
   end
