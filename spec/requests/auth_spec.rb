59bc54b60b2d940d4ab48c491d04521b93a4e3b3
Aristides Perez <aris.a.perez@gmail.com>
2017-12-09 09:48:48 -0500

    rubocop

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -19,7 +19,7 @@ describe 'Auth', :type => :request do
       :password => 'password'
     }
   end
-  let(:request) { post "/auth/sign_in", :params => params }
+  let(:request) { post '/auth/sign_in', :params => params }
 
   context 'sign_in' do
     context 'with valid credentials' do
@@ -28,42 +28,42 @@ describe 'Auth', :type => :request do
       end
 
       it 'returns an access token' do
-        expect(headers.keys).to include("access-token")
+        expect(headers.keys).to include('access-token')
       end
 
       it 'you can use that access token to make requests' do
-        access_headers = headers.slice("Content-Type", "access-token", "token-type", "client", "expiry", "uid")
+        access_headers = headers.slice('Content-Type', 'access-token', 'token-type', 'client', 'expiry', 'uid')
         get "/boxes/#{box.id}/set_owner", :headers => access_headers
         expect(response.status).to eq(200)
       end
     end
 
     context 'with bad credentials' do
-      let(:request) { post "/auth/sign_in", :params => bad_params }
+      let(:request) { post '/auth/sign_in', :params => bad_params }
 
       context 'wrong email' do
-        let(:bad_params) { params.merge!(:email => "notme@me.com") }
+        let(:bad_params) { params.merge!(:email => 'notme@me.com') }
 
         it 'returns an error' do
           expect(subject.status).to eq(401)
-          expect(body).to eq({"errors"=>["Invalid login credentials. Please try again."]})
+          expect(body).to eq('errors' => ['Invalid login credentials. Please try again.'])
         end
 
         it 'does not return an access token' do
-          expect(headers.keys).to_not include("access-token")
+          expect(headers.keys).to_not include('access-token')
         end
       end
 
       context 'wrong password' do
-        let(:bad_params) { params.merge!(:password => "foo") }
+        let(:bad_params) { params.merge!(:password => 'foo') }
 
         it 'returns an error' do
           expect(subject.status).to eq(401)
-          expect(body).to eq({"errors"=>["Invalid login credentials. Please try again."]})
+          expect(body).to eq('errors' => ['Invalid login credentials. Please try again.'])
         end
 
         it 'does not return an access token' do
-          expect(headers.keys).to_not include("access-token")
+          expect(headers.keys).to_not include('access-token')
         end
       end
     end
